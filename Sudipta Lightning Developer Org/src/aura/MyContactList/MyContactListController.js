22b864f7810a889372e1fbf51fb66f6569f4efa2
Sudipta Deb <sudipta.deb@gmail.com>
2017-04-09 01:20:41 -0400

    Added the handler, attribute and markup to the component

--- Sudipta Lightning Developer Org/src/aura/MyContactList/MyContactListController.js
+++ Sudipta Lightning Developer Org/src/aura/MyContactList/MyContactListController.js
@@ -3,7 +3,7 @@
 	myAction : function(component, event, helper) {
 		var action = component.get("c.getContacts");
         action.setCallback(this, function(data) {
-        component.set("v.contacts", data.getReturnValue());
+            component.set("v.contacts", data.getReturnValue());
         });
         $A.enqueueAction(action);
 	}
